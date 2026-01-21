5f8537e1a9f7b9d2f1b78e8f74f1503c818afe23
Ana <163326614+AnaMac3@users.noreply.github.com>
2026-01-21 17:50:17 +0100

    21.01.26 - 17.49h
    Cosas pendientes:
    - Redactar README ❌ ->RESUMIR README Y PASAR COSAS A DEV_DOC (dockerfiles y service architecture)🛠️ - docker compose ✅ - Dockerfiles  ✅🛠️ (hay cosas que igual tienen que ir a dev_doc) - Containers lifecycle ✅ 🛠️ - Services architecture 🛠️(siguiente: NGINX)
    - DEV_DOC ❌ -> Todos los apartados están más o menos, falta resolver dudas apuntadas, añadir comandos de docker, y comprobar usos 🛠️⚠️
    - USER_DOC ❌ -> Services provided by the stack ❌ , Start and stop the project ❌ , Access the website and the administration panel ❌ , Locate and manage credentials 🛠️ ,  check that services are running correctly ❌ 
    - Entender bien el código y comentarlo 🛠️:
    1. docker_compose.yml ✅
    2. Dockerfiles, setup.sh, confs 🛠️ -> estamos en wordpress, AÑADIR configuration
    - Entender y cambiar secrets vs .env ❌
    - Adaptar lo del tunneling de la VM y el navegador a los ordenadores de 42 ❌
    - Dar formato html css a la pág web ❌
    - Entender bien la bbdd: dónde se guardan usuarios, posts, archivos subidos ✅ HAY QUE REDACTARLO BIEN!!! ✅ HACER PRUEBAS DE CADA UNA DE ESTAS COSAS ❌
    -> Tengo que configurar wordpress para que los logs sean persistentes en los volúmenes??  - REVISAR SUBJECT ❌ Apuntes concretos en DEV_DOC.md -> CREO QUE NO... ✅
    -> Cómo cambiar los permisos del user1 para que pueda generar posts ✅  HAY QUE REDACTARLO BIEN!!! ✅ HACER PRUEBAS DESDE LAS DOS VIAS ❌
    - No tiene que haber contraseñas?? repasar subject ❌
    - Funcionamento correcto de todo el sistema:
    - Comprobar comandos de docker y docker compose ❌
    - Hacer pruebas de cambios de permisos, creación de usuarios, creación de posts, subir imágenes... ❌
    
    NOS HEMOS QUEDADO: DEV_DOC -> comandos de docker útiles -> ⚠️ COMPROBAR USOS
    EN 42 -> AVERIGUAR COMO HACER LO DEL DEL SSH ⚠️
    README -> Services architecture -> nginx ⚠️
    ❌❌ POR ALGUNA RAZÓN AHORA DA BAD GATEWAY AL INTENTAR ENTRAR AL NAVEGADOR!!! ❌❌❌

--- srcs/requeriments/mariadb/tools/setup.sh
+++ srcs/requeriments/mariadb/tools/setup.sh
@@ -54,36 +54,36 @@ chown mysql:mysql /run/mysqld
 # and the database must be initialized.
 
 if [ ! -d "$DATADIR/mysql" ]; then
-	echo "Initializing MariaDB database..."
+		echo "Initializing MariaDB database..."
 
-	# Create the internal MariaDB system tables.
-	# This must be done before the server can start.
-	# This process is performed as the mysql user, not as root,
-	# because MariaDB should never run with root privileges.
-	mariadb-install-db --user=mysql --datadir="$DATADIR"
+		# Create the internal MariaDB system tables.
+		# This must be done before the server can start.
+		# This process is performed as the mysql user, not as root,
+		# because MariaDB should never run with root privileges.
+		mariadb-install-db --user=mysql --datadir="$DATADIR"
 
-	# Start MariaDB temporarily in the background ('&')
-	# This allows executing SQL commands (to create databases
-	# and users) while the server is running.
-	# The process ID is stored so the temporary server can be 
-	# stopped once initialization is complete
-	mysqld --user=mysql --datadir="$DATADIR" &
-	pid="$!"
+		# Start MariaDB temporarily in the background ('&')
+		# This allows executing SQL commands (to create databases
+		# and users) while the server is running.
+		# The process ID is stored so the temporary server can be 
+		# stopped once initialization is complete
+		mysqld --user=mysql --datadir="$DATADIR" &
+		pid="$!"
 
-	# Wait until MariaDB is ready to accept connections.
-	# 'mysqladmin ping' returns success once the server is available.
-	until mysqladmin ping --silent; do
-		echo "Waiting for MariaDB to start..."
-		sleep 2
-	done
+		# Wait until MariaDB is ready to accept connections.
+		# 'mysqladmin ping' returns success once the server is available.
+		until mysqladmin ping --silent; do
+			echo "Waiting for MariaDB to start..."
+			sleep 2
+		done
 
-	# Create the application database and user.
-	# Env variables are provided through the .env file and injected
-	# by Docker Compose at runtime.
-	# The MariaDB client is executed as the database root user 
-	# (not password at this stage).
-	# A heredoc is used to pass multiple SQL commands
-	mysql -u root <<EOF
+		# Create the application database and user.
+		# Env variables are provided through the .env file and injected
+		# by Docker Compose at runtime.
+		# The MariaDB client is executed as the database root user 
+		# (not password at this stage).
+		# A heredoc is used to pass multiple SQL commands
+		mysql -u root <<EOF
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
